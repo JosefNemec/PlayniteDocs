6ca96ce949d66ac49e94f9080235bf3f7c4539ae
Josef Nemec <nemecjosef@gmail.com>
2023-05-10 05:48:48 -0700

    desperate

--- publish.ps1
+++ publish.ps1
@@ -20,6 +20,24 @@ if (!(Get-InstalledModule "WinSCP" -EA 0))
 #   $Branch = "master"
 #}
 
+$module = Get-InstalledModule "WinSCP"
+Add-Type -Path (Join-Path $module.AdditionalMetadata.InstalledLocation "lib\netstandard2.0\WinSCPnet.dll")
+ 
+# Setup session options
+$sessionOptions = New-Object WinSCP.SessionOptions -Property @{
+    Protocol = [WinSCP.Protocol]::Ftp
+    HostName = $FtpUrl
+    UserName = $UserName
+    Password = $Password
+}
+
+$session = New-Object WinSCP.Session
+$session.ExecutablePath = (Join-Path $module.AdditionalMetadata.InstalledLocation "bin\WinSCP.exe")
+$session.Open($sessionOptions)
+$session.Dispose()
+
+
+return
 $pw = ConvertTo-SecureString $Password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($UserName, $pw)
 $sessionOption = New-WinSCPSessionOption -HostName $FtpUrl -Protocol Ftp -Credential $credential
