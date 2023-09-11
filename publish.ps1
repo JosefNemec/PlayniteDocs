dca9350aa875887d95bc830e62e6f27420402be1
Josef Nemec <nemecjosef@gmail.com>
2023-09-11 12:06:20 +0200

    Publish script update

--- publish.ps1
+++ publish.ps1
@@ -1,11 +1,12 @@
 #Requires -Version 7
 
 param(
-    [Parameter(Mandatory=$true)] $FtpHost,
+    [Parameter(Mandatory=$true)] $ScpHost,
     [Parameter(Mandatory=$true)] $UserName,
     [Parameter(Mandatory=$true)] $Password,
+    [Parameter(Mandatory=$true)] $Fingerprint,
     [Parameter(Mandatory=$true)] $Branch,
-    [Parameter(Mandatory=$true)] $FtpRootDir
+    [Parameter(Mandatory=$true)] $RootDir
 )
 
 $global:ErrorActionPreference = "Stop"
@@ -16,25 +17,23 @@ if (!(Get-InstalledModule "WinSCP" -EA 0))
 
 $pw = ConvertTo-SecureString $Password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($UserName, $pw)
-$sessionOption = New-WinSCPSessionOption -HostName $FtpHost -Protocol Ftp -Credential $credential
+$sessionOption = New-WinSCPSessionOption -HostName $ScpHost -Protocol Scp -Credential $credential -SshHostKeyFingerprint $Fingerprint
 New-WinSCPSession -SessionOption $sessionOption | Out-Null
 
-$FtpRootDir = $FtpRootDir.TrimEnd('/') + '/'
-$branchPath = $FtpRootDir + $Branch
+$RootDir = $RootDir.TrimEnd('/') + '/'
+$branchPath = $RootDir + $Branch
 if (Test-WinSCPPath $branchPath)
 {
     Remove-WinSCPItem $branchPath -Confirm:$false
 }
 
-Send-WinSCPItem ".\docs\_site\" $branchPath
+New-WinSCPItem $branchPath -ItemType Directory
+Send-WinSCPItem ".\docs\_site\*" $branchPath
 
 # main branch should be also in root in case there are still some old links without branch name
 if ($Branch -eq "main")
 {
-    Start-Process "git" 'branch -r --format "%(refname:lstrip=3)"' -Wait -RedirectStandardOutput ".\gitout.txt"
-    $branches = Get-Content ".\gitout.txt"
-    Get-WinSCPChildItem $FtpRootDir | Where { $branches -notcontains $_.Name } | ForEach { Remove-WinSCPItem ($FtpRootDir + $_.Name) -Confirm:$false }
-    Send-WinSCPItem ".\docs\_site\*" $FtpRootDir
+    Send-WinSCPItem ".\docs\_site\*" $RootDir
 }
 
 Remove-WinSCPSession
